c5d668cfc999159a5fefcdb84407428a729c447e
ManoMax <gatinhogmax123@gmail.com>
2018-06-04 16:17:46 -0300

    Roteiro Oficial 7

---
+++ RoteiroOficial7/R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
@@ -0,0 +1,45 @@
+package adt.linkedList;
+
+public class RecursiveDoubleLinkedListImpl<T> extends
+		RecursiveSingleLinkedListImpl<T> implements DoubleLinkedList<T> {
+
+	protected RecursiveDoubleLinkedListImpl<T> previous;
+
+	public RecursiveDoubleLinkedListImpl() {
+
+	}
+
+	public RecursiveDoubleLinkedListImpl(T data,
+			RecursiveSingleLinkedListImpl<T> next,
+			RecursiveDoubleLinkedListImpl<T> previous) {
+		super(data, next);
+		this.previous = previous;
+	}
+
+	@Override
+	public void insertFirst(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public void removeFirst() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public void removeLast() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	public RecursiveDoubleLinkedListImpl<T> getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(RecursiveDoubleLinkedListImpl<T> previous) {
+		this.previous = previous;
+	}
+
+}
---
+++ RoteiroOficial7/R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/linkedList/RecursiveSingleLinkedListImpl.java
@@ -0,0 +1,70 @@
+package adt.linkedList;
+
+public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
+
+	protected T data;
+	protected RecursiveSingleLinkedListImpl<T> next;
+
+	public RecursiveSingleLinkedListImpl() {
+
+	}
+
+	public RecursiveSingleLinkedListImpl(T data,
+			RecursiveSingleLinkedListImpl<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T search(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public RecursiveSingleLinkedListImpl<T> getNext() {
+		return next;
+	}
+
+	public void setNext(RecursiveSingleLinkedListImpl<T> next) {
+		this.next = next;
+	}
+
+}
