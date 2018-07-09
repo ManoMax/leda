f100742a931bfa2ce43ed3fa1eefcaea2086e96d
ManoMax <gatinhogmax123@gmail.com>
2018-07-09 16:21:28 -0300

    Novo Roteiro

---
+++ RoteiroOficial14/R14-01-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
@@ -0,0 +1,41 @@
+package adt.avltree;
+
+public class AVLCountAndFillImpl<T extends Comparable<T>> extends
+		AVLTreeImpl<T> implements AVLCountAndFill<T> {
+
+	private int LLcounter;
+	private int LRcounter;
+	private int RRcounter;
+	private int RLcounter;
+
+	public AVLCountAndFillImpl() {
+		
+	}
+
+	@Override
+	public int LLcount() {
+		return LLcounter;
+	}
+
+	@Override
+	public int LRcount() {
+		return LRcounter;
+	}
+
+	@Override
+	public int RRcount() {
+		return RRcounter;
+	}
+
+	@Override
+	public int RLcount() {
+		return RLcounter;
+	}
+
+	@Override
+	public void fillWithoutRebalance(T[] array) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+}
