b58d46aeb401d3a20beb3206204f9a372328e568
ManoMax <gatinhogmax123@gmail.com>
2018-07-17 11:50:16 -0300

    Falta VerifyBlackHeight

--- RoteiroOficial16/R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
+++ RoteiroOficial16/R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
@@ -1,6 +1,7 @@
 package adt.rbtree;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
 import adt.bt.Util;
 import adt.rbtree.RBNode.Colour;
 
@@ -11,9 +12,17 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 		this.root = new RBNode<T>();
 	}
 
-	protected int blackHeight() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+	protected int blackHeight() {	
+		return blackeHeight((RBNode<T>) root);
+	}
+
+	private int blackeHeight(RBNode<T> node) {
+		int result = -1;
+
+	      if (!node.isEmpty() && node.getColour() == Colour.BLACK) {
+	         result = 1 + Math.max(height((RBNode<T>) node.getLeft()), height((RBNode<T>) node.getRight()));
+	      }
+      return result;
 	}
 
 	protected boolean verifyProperties() {
@@ -52,29 +61,106 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 	 * be BLACK.
 	 */
 	private boolean verifyChildrenOfRedNodes() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return verifyChildrenOfRedNodes((RBNode<T>) root, true);
 	}
+
+	private boolean verifyChildrenOfRedNodes(RBNode<T> node, boolean verify) {
+		boolean result = verify;
+
+		if (!node.isEmpty()) {
+			if (((RBNode<T>) node).getColour() == Colour.RED) {
+				if (!node.getLeft().isEmpty() && ((RBNode<T>) node.getLeft()).getColour() == Colour.RED) {
+					result = false;
+				}
+				if (!node.getRight().isEmpty() && ((RBNode<T>) node.getRight()).getColour() == Colour.RED) {
+					result = false;
+				}
+			}
+			return (verifyChildrenOfRedNodes((RBNode<T>) node.getLeft(), result)
+					&& verifyChildrenOfRedNodes((RBNode<T>) node.getRight(), result));
+		} else {
+			return result;
+		}
+	}
+	
 
 	/**
 	 * Verifies the black-height property from the root. The method blackHeight
 	 * returns an exception if the black heights are different.
 	 */
 	private boolean verifyBlackHeight() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return verifyBlackHeight((RBNode<T>) root, true);
+	}
+
+	private boolean verifyBlackHeight(RBNode<T> node, boolean verify) {
+		boolean result = verify;
+		
+		if (!node.isEmpty()) {
+			// If 
+		}
+		return false;
 	}
 
 	@Override
 	public void insert(T value) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (value != null) {
+			insertAux((RBNode<T>) root, value);
+		}
 	}
 
+	private void insertAux(RBNode<T> node, T value) {
+		// Disponivel
+		if (node.isEmpty()) {
+			node.setData(value);
+			node.setLeft(new RBNode<T>());
+			node.getLeft().setParent(node);
+			node.setRight(new RBNode<T>());
+			node.getRight().setParent(node);
+			node.setColour(Colour.RED);
+			
+			if (node.getParent() == null) {
+				node.setParent(new BSTNode<>());
+			}
+			//System.out.println(element + " alocado\n");
+			fixUpCase1(node);
+			
+		// Nao Disponivel
+		} else {
+			// Elemento menor que Atual node
+			if (node.getData().compareTo(value) > 0) {
+				//System.out.println(element + " esquerda");
+				insertAux((RBNode<T>) node.getLeft(), value);
+
+				// Elemento maior que Atual node
+			} else if (node.getData().compareTo(value) < 0) {
+				//System.out.println(element + " direita");
+				insertAux((RBNode<T>) node.getLeft(), value);
+			}
+		}
+	}
+
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public RBNode<T>[] rbPreOrder() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		RBNode[] array = new RBNode[this.size()];
+	      if (this.isEmpty())
+	         return array;
+	      preOrder(array, (RBNode<T>) root, 0);
+	      return array;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private int preOrder(RBNode[] array, RBNode<T> node, int index) {
+		array[index++] = (RBNode) node;
+
+	      if (!node.getLeft().isEmpty())
+	         index = preOrder(array, (RBNode<T>) node.getLeft(), index);
+
+	      if (!node.getRight().isEmpty())
+	         index = preOrder(array, (RBNode<T>) node.getRight(), index);
+
+	      return index;
 	}
 
 	// FIXUP methods
