fd71814097382f1eab25fe03bc3aaca6a8f42726
ManoMax <gatinhogmax123@gmail.com>
2018-08-01 09:10:30 -0300

    Generics Java

--- RoteiroDeRevisao2/RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/repositorio/RepositorioConta.java
+++ RoteiroDeRevisao2/RR2-01-Roteiro-Revisao-Generics-environment/src/main/java/repositorio/RepositorioConta.java
@@ -12,21 +12,13 @@ public class RepositorioConta {
 		contas = new ContaAbstrata[20];
 	}
 
-<<<<<<< HEAD
-	private int procurarIndice(ContaAbstrata cliente) {
-=======
-	private int procurarIndice(ContaAbstrata conta) {
->>>>>>> 98e87768c1cbec382073741311da90b4af5b6aae
+	private int procurarIndice(ContaAbstrata cli) {
 		int i = 0;
 		int resp = -1;
 		boolean achou = false;
 
 		while ((i < indice) && !achou) {
-<<<<<<< HEAD
-			if ((contas[i]).equals(cliente)) {
-=======
-			if ((contas[i]).equals(conta)) {
->>>>>>> 98e87768c1cbec382073741311da90b4af5b6aae
+			if ((contas[i]).equals(cli)) {
 				resp = i;
 				achou = true;
 			}
@@ -35,17 +27,10 @@ public class RepositorioConta {
 		return resp;
 	}
 
-<<<<<<< HEAD
-	public boolean existe(ContaAbstrata cliente) {
+	public boolean existe(ContaAbstrata cli) {
 		boolean resp = false;
 
-		int i = this.procurarIndice(cliente);
-=======
-	public boolean existe(ContaAbstrata conta) {
-		boolean resp = false;
-
-		int i = this.procurarIndice(conta);
->>>>>>> 98e87768c1cbec382073741311da90b4af5b6aae
+		int i = this.procurarIndice(cli);
 		if (i != -1) {
 			resp = true;
 		}
@@ -53,34 +38,23 @@ public class RepositorioConta {
 		return resp;
 	}
 
-<<<<<<< HEAD
 	public void inserir(ContaAbstrata novoCliente) {
 		contas[indice] = novoCliente;
-=======
-	public void inserir(ContaAbstrata novaConta) {
-		contas[indice] = novaConta;
->>>>>>> 98e87768c1cbec382073741311da90b4af5b6aae
-		this.indice++;
+		indice = indice + 1;
 	}
 
-	public void atualizar(ContaAbstrata conta) {
-		int i = procurarIndice(conta);
+	public void atualizar(ContaAbstrata cliente) {
+		int i = procurarIndice(cliente);
 		if (i != -1) {
-			contas[i] = conta;
+			contas[i] = cliente;
 		} else {
 			throw new RuntimeException("Conta nao encontrada");
 		}
 	}
 
-<<<<<<< HEAD
-	public ContaAbstrata procurar(ContaAbstrata cliente) {
+	public ContaAbstrata procurar(ContaAbstrata cli) {
 		ContaAbstrata resp = null;
-		int i = this.procurarIndice(cliente);
-=======
-	public ContaAbstrata procurar(ContaAbstrata conta) {
-		ContaAbstrata resp = null;
-		int i = this.procurarIndice(conta);
->>>>>>> 98e87768c1cbec382073741311da90b4af5b6aae
+		int i = this.procurarIndice(cli);
 		if (i != -1) {
 			resp = contas[i];
 		} else {
@@ -90,13 +64,8 @@ public class RepositorioConta {
 		return resp;
 	}
 
-<<<<<<< HEAD
-	public void remover(ContaAbstrata cliente) {
-		int i = this.procurarIndice(cliente);
-=======
-	public void remover(ContaAbstrata conta) {
-		int i = this.procurarIndice(conta);
->>>>>>> 98e87768c1cbec382073741311da90b4af5b6aae
+	public void remover(ContaAbstrata cli) {
+		int i = this.procurarIndice(cli);
 		if (i != -1) {
 			contas[i] = contas[indice - 1];
 			contas[indice - 1] = null;
